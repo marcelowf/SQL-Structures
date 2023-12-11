df688ad39ccf900c57d619b27b2b2639dfba678d
Marcelo Wzorek F <103009053+marcelowf@users.noreply.github.com>
2023-12-11 15:44:52 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -1,131 +1,143 @@
 -- 1) Quantidade de ingressos vendidos
-select sum(quantidade) as "Quantidade Ingressos vendidos" from parque.compra;
+SELECT SUM(quantidade) AS "Quantidade Ingressos Vendidos" FROM parque.compra;
 
 -- 2) Registro do show com seu nome e atores/atrizes escalados
-select shows.id as "ID do Show", atracao.nome as "Nome do Show", registro_shows.data_realizado as "Data de realização", pessoa.nome as "Ator/Atriz Escalado" from atracao
-inner join shows on atracao.id = shows.id
-inner join registro_shows on shows.id = registro_shows.id_show
-inner join escala_show on registro_shows.id = escala_show.id_reg_show
-inner join pessoa on escala_show.cpf_ator = pessoa.cpf
-order by data_realizado;
+SELECT shows.id AS "ID do Show", atracao.nome AS "Nome do Show", registro_shows.data_realizado AS "Data de Realização", pessoa.nome AS "Ator/Atriz Escalado"
+FROM atracao
+INNER JOIN shows ON atracao.id = shows.id
+INNER JOIN registro_shows ON shows.id = registro_shows.id_show
+INNER JOIN escala_show ON registro_shows.id = escala_show.id_reg_show
+INNER JOIN pessoa ON escala_show.cpf_ator = pessoa.cpf
+ORDER BY data_realizado;
 
 -- 3) Faturamento total Ingressos
-select sum(valor) as "Faturamento Ingressos" from pagamento;
+SELECT SUM(valor) AS "Faturamento Ingressos" FROM pagamento;
 
 -- 4) Faturamento Total Lojas
-select sum(valor) "Faturamento Lojas" from nota;
+SELECT SUM(valor) AS "Faturamento Lojas" FROM nota;
 
 -- 5) Ranking de venda de ingressos por vendedor
-select pessoa.nome, pessoa.cpf, count(compra.cpf_vendedor) as Quantidade_vendida from compra
-inner join pessoa on compra.cpf_cliente = pessoa.cpf
-group by compra.cpf_vendedor
-order by Quantidade_vendida desc;
+SELECT pessoa.nome, pessoa.cpf, COUNT(compra.cpf_vendedor) AS "Quantidade Vendida"
+FROM compra
+INNER JOIN pessoa ON compra.cpf_cliente = pessoa.cpf
+GROUP BY compra.cpf_vendedor
+ORDER BY Quantidade_Vendida DESC;
 
 -- 6) Média de salário geral
-select avg(salario) as "Média de Salário" from funcionario;
+SELECT AVG(salario) AS "Média de Salário" FROM funcionario;
 
 -- 7) Média de salário por cargo
-select Avg(salario), 
-Case 
- When operador.cpf Is Not null then "Operador"
- When atores.cpf Is Not null then "Ator"
- When vendedor_ingressos.cpf Is Not Null then "Vendedor Ingressos"
- When logista.cpf Is Not Null then "Logista"
- Else "Não Definido" End as Cargo 
-from Funcionario 
-left join operador on funcionario.cpf = operador.cpf
-left join atores on funcionario.cpf = atores.cpf
-left  join vendedor_ingressos on funcionario.cpf = vendedor_ingressos.cpf
-left  join logista on funcionario.cpf = logista.cpf
-Group By Cargo;
+SELECT AVG(salario) AS "Média de Salário", 
+  CASE 
+    WHEN operador.cpf IS NOT NULL THEN 'Operador'
+    WHEN atores.cpf IS NOT NULL THEN 'Ator'
+    WHEN vendedor_ingressos.cpf IS NOT NULL THEN 'Vendedor Ingressos'
+    WHEN logista.cpf IS NOT NULL THEN 'Logista'
+    ELSE 'Não Definido'
+  END AS Cargo 
+FROM funcionario 
+LEFT JOIN operador ON funcionario.cpf = operador.cpf
+LEFT JOIN atores ON funcionario.cpf = atores.cpf
+LEFT JOIN vendedor_ingressos ON funcionario.cpf = vendedor_ingressos.cpf
+LEFT JOIN logista ON funcionario.cpf = logista.cpf
+GROUP BY Cargo;
 
 -- 8) Quantidade de Funcionários por Cargo
-select count(*) "Quantidade de Funcionários", 
-Case 
- When operador.cpf Is Not null then "Operador"
- When atores.cpf Is Not null then "Ator"
- When vendedor_ingressos.cpf Is Not Null then "Vendedor Ingressos"
- When logista.cpf Is Not Null then "Logista"
- Else "Não Definido" End as Cargo 
-from Funcionario 
-left join operador on funcionario.cpf = operador.cpf
-left join atores on funcionario.cpf = atores.cpf
-left  join vendedor_ingressos on funcionario.cpf = vendedor_ingressos.cpf
-left  join logista on funcionario.cpf = logista.cpf
-Group By Cargo;
+SELECT COUNT(*) AS "Quantidade de Funcionários", 
+  CASE 
+    WHEN operador.cpf IS NOT NULL THEN 'Operador'
+    WHEN atores.cpf IS NOT NULL THEN 'Ator'
+    WHEN vendedor_ingressos.cpf IS NOT NULL THEN 'Vendedor Ingressos'
+    WHEN logista.cpf IS NOT NULL THEN 'Logista'
+    ELSE 'Não Definido'
+  END AS Cargo 
+FROM funcionario 
+LEFT JOIN operador ON funcionario.cpf = operador.cpf
+LEFT JOIN atores ON funcionario.cpf = atores.cpf
+LEFT JOIN vendedor_ingressos ON funcionario.cpf = vendedor_ingressos.cpf
+LEFT JOIN logista ON funcionario.cpf = logista.cpf
+GROUP BY Cargo;
 
 -- 9) Soma de faturamento por logista 
-select l.cpf as cpf_vendedor, p.nome as vendedor, sum(valor) as "Valor toal de vendas" from parque.nota as n
-inner join logista as l on n.cpf_logista = l.cpf
-inner join pessoa as p on l.cpf = p.cpf
-group by cpf_logista;
+SELECT l.cpf AS cpf_vendedor, p.nome AS vendedor, SUM(valor) AS "Valor Total de Vendas"
+FROM parque.nota AS n
+INNER JOIN logista AS l ON n.cpf_logista = l.cpf
+INNER JOIN pessoa AS p ON l.cpf = p.cpf
+GROUP BY cpf_logista;
 
 -- 10) Ranking de vendas de produtos por loja
-select l.id "ID loja", l.nome as "Nome da Loja", count(id_produto) as "Quantidade de produtos vendidos" from nota as n
-inner join lojas as l on n.id_loja = l.id
-inner join produtos_nota as pn on n.id = pn.id_nota
-group by id_loja
-order by count(id_produto) desc;
+SELECT l.id AS "ID Loja", l.nome AS "Nome da Loja", COUNT(id_produto) AS "Quantidade de Produtos Vendidos"
+FROM nota AS n
+INNER JOIN lojas AS l ON n.id_loja = l.id
+INNER JOIN produtos_nota AS pn ON n.id = pn.id_nota
+GROUP BY id_loja
+ORDER BY COUNT(id_produto) DESC;
 
 -- 11)  Ranking de faturamento ingressos por vendedor
-select c.cpf_vendedor, pe.nome as nome_vendedor, sum(p.valor) as "Faturamento" from compra as c
-inner join pagamento as p on c.id = p.id_compra
-inner join pessoa as pe on c.cpf_vendedor = pe.cpf
-group by c.cpf_vendedor
-order by sum(p.valor) desc;
+SELECT c.cpf_vendedor, pe.nome AS nome_vendedor, SUM(p.valor) AS "Faturamento"
+FROM compra AS c
+INNER JOIN pagamento AS p ON c.id = p.id_compra
+INNER JOIN pessoa AS pe ON c.cpf_vendedor = pe.cpf
+GROUP BY c.cpf_vendedor
+ORDER BY SUM(p.valor) DESC;
 
 -- 12) Shows que passaram de 80% da capacidade máxima 
-select *, rs.lotacao / s.lotacao_max as percentual from shows as s
-inner join registro_shows as rs on rs.id_show = s.id having percentual >= 0.80;
+SELECT *, rs.lotacao / s.lotacao_max AS percentual
+FROM shows AS s
+INNER JOIN registro_shows AS rs ON rs.id_show = s.id
+HAVING percentual >= 0.80;
 
--- 13) Quantidade de atraçõespor localidade
-select localidade, count(*) as quantidade from atracao 
-group by localidade;
+-- 13) Quantidade de atrações por localidade
+SELECT localidade, COUNT(*) AS quantidade FROM atracao 
+GROUP BY localidade;
 
--- 14) Maior idade minima por localidade 
-select localidade, max(idade_minima) as idade_minima_max from atracao 
-group by localidade;
+-- 14) Maior idade mínima por localidade 
+SELECT localidade, MAX(idade_minima) AS idade_minima_max FROM atracao 
+GROUP BY localidade;
 
 -- 15) Quais produtos pertencem a quais lojas
-select  p.id as id_produto, p.nome as nome_produto, p.preco as preco_produto, l.id as id_loja, l.nome as nome_loja from lojas as l
-inner join loja_produtos as lp on l.id = lp.id_loja
-inner join produtos as p on lp.id_produto = p.id;
+SELECT p.id AS "ID Produto", p.nome AS "Nome do Produto", p.preco AS "Preço do Produto", l.id AS "ID Loja", l.nome AS "Nome da Loja"
+FROM lojas AS l
+INNER JOIN loja_produtos AS lp ON l.id = lp.id_loja
+INNER JOIN produtos AS p ON lp.id_produto = p.id;
 
--- 16 Media de preço de produtos por loja
-select l.id as id_loja, l.nome as nome_loja, round(avg(p.preco),2) as "Média de preço dos produtos" from lojas as l
-inner join loja_produtos as lp on l.id = lp.id_loja
-inner join produtos as p on lp.id_produto = p.id
-group by id_loja;
+-- 16) Média de preço de produtos por loja
+SELECT l.id AS "ID Loja", l.nome AS "Nome da Loja", ROUND(AVG(p.preco), 2) AS "Média de Preço dos Produtos"
+FROM lojas AS l
+INNER JOIN loja_produtos AS lp ON l.id = lp.id_loja
+INNER JOIN produtos AS p ON lp.id_produto = p.id
+GROUP BY id_loja;
 
--- 17) Nome e cargo de funcionarios que ganham acima de 1800 R$
-select pessoa.cpf, pessoa.nome, funcionario.salario, 
-Case 
- When operador.cpf Is Not null then "Operador"
- When atores.cpf Is Not null then "Ator"
- When vendedor_ingressos.cpf Is Not Null then "Vendedor Ingressos"
- When logista.cpf Is Not Null then "Logista"
- Else "Não Definido" End as Cargo from funcionario 
-inner join pessoa on pessoa.cpf = funcionario.cpf
-left join operador on funcionario.cpf = operador.cpf
-left join atores on funcionario.cpf = atores.cpf
-left  join vendedor_ingressos on funcionario.cpf = vendedor_ingressos.cpf
-left  join logista on funcionario.cpf = logista.cpf
-where salario >= 1800
-order by salario desc;
+-- 17) Nome e cargo de funcionários que ganham acima de 1800 R$
+SELECT pessoa.cpf, pessoa.nome, funcionario.salario, 
+  CASE 
+    WHEN operador.cpf IS NOT NULL THEN 'Operador'
+    WHEN atores.cpf IS NOT NULL THEN 'Ator'
+    WHEN vendedor_ingressos.cpf IS NOT NULL THEN 'Vendedor Ingressos'
+    WHEN logista.cpf IS NOT NULL THEN 'Logista'
+    ELSE 'Não Definido'
+  END AS Cargo
+FROM funcionario 
+INNER JOIN pessoa ON pessoa.cpf = funcionario.cpf
+LEFT JOIN operador ON funcionario.cpf = operador.cpf
+LEFT JOIN atores ON funcionario.cpf = atores.cpf
+LEFT JOIN vendedor_ingressos ON funcionario.cpf = vendedor_ingressos.cpf
+LEFT JOIN logista ON funcionario.cpf = logista.cpf
+WHERE salario >= 1800
+ORDER BY salario DESC;
 
 -- 18) Dia que mais foram comprados ingressos
-select data_checkin as data, sum(quantidade) as quantidade_vendida 
-from compra 
-group by data_checkin 
-order by quantidade_vendida desc 
-limit 1;
+SELECT data_checkin AS data, SUM(quantidade) AS quantidade_vendida 
+FROM compra 
+GROUP BY data_checkin 
+ORDER BY quantidade_vendida DESC 
+LIMIT 1;
 
 -- 19) Faturamento Total
-select sum(soma) as "Faturamento Total" from 
-((select sum(valor) as soma from pagamento)
-union
-(select sum(valor) as soma from nota)) as x;
+SELECT SUM(soma) AS "Faturamento Total" 
+FROM ((SELECT SUM(valor) AS soma FROM pagamento)
+UNION
+(SELECT SUM(valor) AS soma FROM nota)) AS x;
 
-
--- 20) Depesas total com salários
-select round(sum(salario),2) as "Soma dos salários" from funcionario;
+-- 20) Despesas total com salários
+SELECT ROUND(SUM(salario), 2) AS "Soma dos Salários" FROM funcionario;
